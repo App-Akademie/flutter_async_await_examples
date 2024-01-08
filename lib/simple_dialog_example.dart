8a85812f2b748dbed5bb2e9675635d835b206c92
Kai Hatje <kai.hatje@app-akademie.com>
2024-01-08 20:54:30 +0100

    Add Text showing current choice.

--- lib/simple_dialog_example.dart
+++ lib/simple_dialog_example.dart
@@ -18,30 +18,53 @@ class AlertDialogExampleApp extends StatelessWidget {
   }
 }
 
-class DialogExample extends StatelessWidget {
+class DialogExample extends StatefulWidget {
   const DialogExample({super.key});
 
   @override
+  State<DialogExample> createState() => _DialogExampleState();
+}
+
+class _DialogExampleState extends State<DialogExample> {
+  String dialogResult = "";
+
+  @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const Text('AlertDialog description'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: getUserChoice,
+          child: const Text('Show Dialog'),
         ),
-      ),
-      child: const Text('Show Dialog'),
+        const SizedBox(height: 16),
+        Text("Dialog result: \"$dialogResult\"")
+      ],
     );
+  }
+
+  void getUserChoice() async {
+    dialogResult = await showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('AlertDialog Title'),
+            content: const Text('AlertDialog description'),
+            actions: <Widget>[
+              TextButton(
+                // The Dialog will return the result of this in its Future.
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                // The Dialog will return the result of this in its Future.
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        ) ??
+        "None";
+    // Actually trigger rebuild so changes will be shown.
+    setState(() {});
   }
 }
